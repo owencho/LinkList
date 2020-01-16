c2be18813cfe669f2a3c0f903451e96aecdc1daa
owencho <owencho200@gmail.com>
2020-01-16 14:33:00 +0800

    modified for the padding on if else statement

--- src/NetworkNode.c
+++ src/NetworkNode.c
@@ -1,14 +1,14 @@
 #include "NetworkNode.h"
 
 char* getNodeName(NetworkNode * node){
-    if (node == NULL)
+    if(node == NULL)
         return NULL;
 
     return node->name;
 }
 
 List * getIteratorOfLinks(NetworkNode * node){
-    if (node == NULL)
+    if(node == NULL)
         return NULL;
 
     return node->links;
